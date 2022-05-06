95348bf4310cd0d2448f3389645ecb4125d2dc60
Beibarsov <BarsStudio@gmail.com>
2022-05-06 19:23:56 +0800

    Создана комманда загрузки

---
+++ SF18/CommandDownloadVideo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF18
+{
+    public class CommandDownloadVideo : Command
+    {
+        Receiver receiver;
+
+        public CommandDownloadVideo(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public override async Task Run()
+        {
+            await receiver.DownloadVideo();
+        }
+    }
+}
